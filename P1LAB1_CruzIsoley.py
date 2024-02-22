841a44feba25bfde00727ddc9e150500faddab97
Isoley <160941225+Isoley@users.noreply.github.com>
2024-02-22 14:47:22 -0500

    Add files via upload
    First submission!

---
+++ P1LAB1_CruzIsoley.py
@@ -0,0 +1,8 @@
+# Isoley
+# 2/22/2024
+# P1LAB1
+# This program will display a message welcoming this user to this course!
+
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print("Hello " + fname + " " + lname + " Welcome to CTI-110!")
